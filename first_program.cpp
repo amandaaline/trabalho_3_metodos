cba026cece9cc7889f746c94726a82c18c86ce4a
amandaaline <amandaaline3@gmail.com>
2018-10-10 14:48:54 -0300

    First program

---
+++ first_program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+ /**
+ * @brief Programa inicial para teste
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+
+int main(int argc, char const *argv[])
+{
+    cout << "Testando!"; 
+    
+    return 0;
+}
